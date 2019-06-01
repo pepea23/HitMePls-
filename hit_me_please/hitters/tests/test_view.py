0562fa47c5ac5df51c1c1797f8ecf377338b2e7d
Ittidate <pepea.23@live.com>
2019-06-01 15:03:56 +0700

    [ADD] feature email and test and save email data to BE

--- hit_me_please/hitters/tests/test_view.py
+++ hit_me_please/hitters/tests/test_view.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
+from ..models import Hitter
+
 
 class LandingPageViewTest(TestCase):
     def test_view_should_have_from_wit_enail_field_and_submit_button(self):
         response = self.client.get('/')
         expected = '<form action = "." method ="post">'
-        self.assertContains(response,expected,status_code=200)
-    
+        self.assertContains(response, expected, status_code=200)
+        expected = '<input type="hidden" name="csrfmiddlewaretoken"'
+        self.assertContains(response, expected, status_code=200)
         expected = '<input type ="email" />'
-
-        self.assertContains(response,expected,status_code=200)
+        self.assertContains(response, expected, status_code=200)
         expected = '<button type ="submit" >Sunbmit </button>'
+        self.assertContains(response, expected, status_code=200)
 
-        self.assertContains(response,expected,status_code=200)
+    def test_view_should_have_submit_form(self):
+
+        data = {
+            'email': 'pepea.23@live.com'
+        }
+        response = self.client.post('/', data=data)
+        self.assertEqual(response.status_code,200)
+        count = Hitter.objects.filter(email="pepea.23@live.com").count()
+        self.assertEqual(count, 1)
--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,9 +1,17 @@
 # Create your views here.
+from .models import Hitter
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+
 class LandingPageView(View):
     def get(self,request):
         return render(
             request,
             'index.html'
         )
+    def post(self,request):
+        email = request.POST['email']
+
+        Hitter.objects.create(email=email)
+        return HttpResponse()
