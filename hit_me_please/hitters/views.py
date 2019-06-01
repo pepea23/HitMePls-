a97723c8ede7e4d329a821eb9486889ddfae4d3b
Ittidate <pepea.23@live.com>
2019-06-01 14:31:00 +0700

    [UPDATE] Refactor html

--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
 class LandingPageView(View):
-    def get(self,reuest):
-        return HttpResponse('<form action = "." method ="post"><input type ="email" /> <button type ="submit" >Sunbmit </button> </form>')
+    def get(self,request):
+        return render(
+            request,
+            'index.html'
+        )
