1cf65242fcddc5b583ea3db789bf43ef4dce72b5
Ittidate <pepea.23@live.com>
2019-06-01 11:19:36 +0700

    [UPDATE] model + test

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Hitter
 
 # Create your tests here.
+class HitterTest(TestCase):
+    def test_hitter_model_should_have_email_field(self):
+        hitter=Hitter.objects.create( email = "pepea.23@live.com")
+        self.assertEqual(hitter.email,'pepea.23@live.com')
+
+
